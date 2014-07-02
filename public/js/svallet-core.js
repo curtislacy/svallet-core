cbd6488fb57085ff6b29ff1f18e22c0e26388e1b
Curtis Lacy <curtis.lacy@grgcomponents.com>
2014-07-01 22:03:26 -0400

    Added a total balance query to the MultiAddressSvallet.

--- public/js/svallet-core.js
+++ public/js/svallet-core.js
@@ -19,6 +19,19 @@ MultiAddressSvallet.prototype.getCoinData = function( currency ) {
 	}
 	return null;
 }
+MultiAddressSvallet.prototype.getTotalBalance = function( currency ) {
+	var sum = 0;
+	for( var k in this.svallets )
+	{
+		if( this.svallets.hasOwnProperty( k ))
+		{
+			var balance = this.svallets[ k ].svalletData.balances.get( currency );
+			if( balance )
+				sum += balance;
+		}
+	}
+	return sum;
+}
 MultiAddressSvallet.prototype.add = function( address ) {
 	var multiSvallet = this;
 	var newSvallet = new SingleAddressSvallet();
